d377c186e958a688707d36c92f227e2d1dfc4491
Daniel-k-dev <juyeon10120@naver.com>
2025-01-20 01:25:52 +0900

    chore(app): 불필요한 초기파일 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import klaytnConfig from './common/core/blockchain/klaytnApi/config';
+import slackBotConfig from './common/core/external-api/slack/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'production' ? '.env' : ['.env', '.env.local'],
+      isGlobal: true,
+      load: [klaytnConfig, slackBotConfig],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
