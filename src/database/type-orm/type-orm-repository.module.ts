0099a0e7d1449251303ddab2e3641df5c3ab3b89
Daniel-k-dev <juyeon10120@naver.com>
2025-02-05 15:16:46 +0900

    feat(database): typeorm 레포지토리 모듈 설정

---
+++ src/database/type-orm/type-orm-repository.module.ts
@@ -0,0 +1,84 @@
+import { Module, Provider } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from '@src/config/configs';
+import { RepositoryToken } from '@src/core/constant/repository';
+import {
+  AverageCostEntity,
+  BirthDateEntity,
+  DeliveryPersonMatchedDateEntity,
+  DepartureEntity,
+  DestinationEntity,
+  JoinDateEntity,
+  OrderEntity,
+  ProductEntity,
+  ProfileImageEntity,
+  ReceiverEntity,
+  SenderEntity,
+  TransportationEntity,
+  UserEntity,
+} from './entity';
+import {
+  AverageCostRepository,
+  DeliveryPersonMatchedDateRepository,
+  LocationRepository,
+  OrderParticipantRepository,
+  OrderRepository,
+  ReceiverRepository,
+  UserRepository,
+} from './repository';
+import { TypeOrmOption } from './type-orm-option';
+
+const entities = [
+  AverageCostEntity,
+  BirthDateEntity,
+  DeliveryPersonMatchedDateEntity,
+  DepartureEntity,
+  DestinationEntity,
+  JoinDateEntity,
+  OrderEntity,
+  ProductEntity,
+  ProfileImageEntity,
+  ReceiverEntity,
+  SenderEntity,
+  TransportationEntity,
+  UserEntity,
+];
+
+const repositories: Provider[] = [
+  {
+    provide: RepositoryToken.AVERAGE_COST_REPOSITORY,
+    useClass: AverageCostRepository,
+  },
+  {
+    provide: RepositoryToken.DELIVERY_PERSON_MATCHED_DATE_REPOSITORY,
+    useClass: DeliveryPersonMatchedDateRepository,
+  },
+  {
+    provide: RepositoryToken.LOCATION_REPOSITORY,
+    useClass: LocationRepository,
+  },
+  { provide: RepositoryToken.ORDER_REPOSITORY, useClass: OrderRepository },
+  {
+    provide: RepositoryToken.ORDER_PARTICIPANT_REPOSITORY,
+    useClass: OrderParticipantRepository,
+  },
+  {
+    provide: RepositoryToken.RECEIVER_REPOSITORY,
+    useClass: ReceiverRepository,
+  },
+  { provide: RepositoryToken.USER_REPOSITORY, useClass: UserRepository },
+];
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmOption,
+    }),
+    TypeOrmModule.forFeature(entities),
+    ConfigModule.forFeature(typeOrmConfig),
+  ],
+  providers: [...repositories],
+  exports: [...repositories],
+})
+export class TypeOrmRepositoryModule {}
