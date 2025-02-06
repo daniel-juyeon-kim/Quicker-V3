8e3d2a417324849459aa80d72e65a28d3929abf3
Daniel-k-dev <juyeon10120@naver.com>
2025-02-06 15:14:30 +0900

    feat(middleware): 공통 미들웨어 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setMiddleware } from './core/middleware/set-middleware';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  setMiddleware(app);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
