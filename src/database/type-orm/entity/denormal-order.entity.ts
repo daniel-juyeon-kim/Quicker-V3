a6cdc87a947ae280888634f6a0567f4adb228c39
Daniel-k-dev <juyeon10120@naver.com>
2025-09-14 09:35:12 +0900

    refactor(database): DenormalOrderEntity의 인덱스 설정 오류 수정

--- src/database/type-orm/entity/denormal-order.entity.ts
+++ src/database/type-orm/entity/denormal-order.entity.ts
@@ -11,7 +11,7 @@ import { UserEntity } from '.';
 import { OrderParticipantEntity } from './order-participant.entity';
 
 @Entity({ name: 'order', orderBy: { id: 'DESC' } })
-@Index('idx_requester_orderId', ['requester', 'id'])
+@Index('idx_deliveryPerson_orderId', ['deliveryPerson', 'id'])
 export class DenormalOrderEntity {
   @PrimaryGeneratedColumn()
   id: number;
